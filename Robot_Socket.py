a24475e871b5f9d50a667c7c56f011e08aa5a9e8
Adam <arb5012@g.rit.edu>
2024-04-01 00:42:13 -0400

    REvised for monday

--- Robot_Socket.py
+++ Robot_Socket.py
@@ -1,1 +1,22 @@
 import socket
+
+# Define the IP address and port to connect to
+HOST = '127.0.0.1'
+PORT = 5024
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect((HOST, PORT))
+
+# Send data to the server
+data_to_send = "Hello from Python!"
+client_socket.send(data_to_send.encode())
+
+# Receive data from the server
+received_data = client_socket.recv(1024).decode()
+print("Received data from RobotStudio:", received_data)
+
+# Close the socket
+client_socket.close()
