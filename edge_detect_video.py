9a8fb75d13646403d8c656070c4ff0936ac02525
Ryan McGee <ramtech51@comcast.net>
2024-04-19 10:27:53 -0400

    Fixed picam2 support w/ jetson

--- edge_detect_video.py
+++ edge_detect_video.py
@@ -82,7 +82,7 @@ def edge_detect(img):
     return debug_frame
 
 
-cap = cv.VideoCapture(0, cv.CAP_V4L2)
+cap = cv.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink")
 if not cap.isOpened():
     print("Failed to open camera :(")
     exit(-1)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import cv2 as cv
 from visionops import process
 
-vc = cv.VideoCapture(0)
+vc = cv.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink")
 
 while(cv.waitKey(5) != ord('q')):
     arr = process(vc)
